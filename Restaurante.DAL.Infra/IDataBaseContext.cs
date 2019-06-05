03d586ba942ebd6cd002540745c5ecec7ce6fec4
tizopone <ti@zopone.com.br>
2019-06-05 11:28:19 -0300

    Adicionando mais assinaturas a interface

--- Restaurante.DAL.Infra/IDataBaseContext.cs
+++ Restaurante.DAL.Infra/IDataBaseContext.cs
@@ -10,9 +10,9 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Restaurante.DAL.Infra
 {
-    interface IDataBaseContext : IDisposable
+    public interface IDataBaseContext : IDisposable
     {
-        ChangeTracker ChangeTracker {get; }
+        ChangeTracker ChangeTracker { get; }
         DatabaseFacade Database { get; }
         IModel Model { get; }
 
@@ -23,6 +23,38 @@ namespace Restaurante.DAL.Infra
         void AddRange(IEnumerable<object> entities);
         void AddRange(params object[] entities);
         Task AddRangeAsync(IEnumerable<object> entities, CancellationToken cancellationToken = default(CancellationToken));
+        Task AddRangeAsync(params object[] entities);
+        EntityEntry Attach(object entity);
+        EntityEntry<TEntity> Attach<TEntity>(TEntity entity) where TEntity : class;
+        void AttachRange(IEnumerable<object> entities);
+        void AttachRange(params object[] entities);
+        void Dispose();
+        EntityEntry Entry(object entity);
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        bool Equals(object obj);
+        object Find(Type entityType, params object[] keyValues);
+        TEntity Find<TEntity>(params object[] keyValues) where TEntity : class;
+        Task<object> FindAsync(Type entityType, object[] keyValues, CancellationToken cancellationToken);
+        Task<object> FindAsync(Type entityType, params object[] keyValues);
+        Task<TEntity> FindAsync<TEntity>(object[] keyValues, CancellationToken cancellationToken) where TEntity : class;
+        Task<TEntity> FindAsync<TEntity>(params object[] keyValues) where TEntity : class;
+        int GetHashCode();
+        DbQuery<TQuery> Query<TQuery>() where TQuery : class;
+        EntityEntry Remove(object entity);
+        EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class;
+        void RemoveRange(IEnumerable<object> entities);
+        void RemoveRange(params object[] entities);
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        string ToString();
+        EntityEntry Update(object entity);
+        EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
+        void UpdateRange(IEnumerable<object> entities);
+        void UpdateRange(params object[] entities);
+        void SetModified(object entity);
 
 
 
