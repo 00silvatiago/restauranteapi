2f5faec473b9606a5115bc87ebe7fb201b9b31e9
00silvatiago <51232585+00silvatiago@users.noreply.github.com>
2019-06-06 13:01:36 -0300

    Subindo todos os arquivos novamente, poius faltou o HELPER. Teste Post

---
+++ Restaurante.Helpers/ResponseContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurante.Helpers
+{
+    public class ResponseContent
+    {
+        public object Object { get; set; }
+        public string Message { get; set; }
+        public bool Status { get; set; }
+        public int Skype { get; set; }
+        public int Take { get; set; }
+    }
+}
